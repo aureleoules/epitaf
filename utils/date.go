368d94cc57c4438c8e0833279f9cdd1e038745c6
Aurèle Oulès <aurele@oules.com>
2020-11-11 14:00:02 +0100

    added comment

--- utils/date.go
+++ utils/date.go
@@ -2,6 +2,7 @@ package utils
 
 import "time"
 
+// TruncateDate truncates date by removing hours, minutes and seconds
 func TruncateDate(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }

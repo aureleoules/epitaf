ea6d54f12106c5ac6f3a410ee78f16ea819ac5da
Aurèle Oulès <aurele@oules.com>
2021-09-09 23:18:58 +0200

    zeus group slug

--- api/zeus.go
+++ api/zeus.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/aureleoules/epitaf/lib/zeus"
 	"github.com/gin-gonic/gin"
@@ -11,20 +13,27 @@ import (
 func handleZeus() {
 	users := router.Group("/zeus")
 
-	users.GET("/feed/:group_id", getZeusFeed)
+	users.GET("/feed/:slug", getZeusFeed)
 }
 
 // @Summary Get Zeus ICS feed
 // @Tags zeus
 // @Description Get Zeus ICS feed
-// @Param group_id body string true "group_id"
+// @Param slug query string true "slug"
 // @Success 200	"OK"
 // @Failure 406	"Not acceptable"
-// @Router /zeus/feed/{group_id} [GET]
+// @Router /zeus/feed/{slug} [GET]
 func getZeusFeed(c *gin.Context) {
 	client := zeus.NewClient(nil)
 
-	ics, err := client.GetICS(c.Param("group_id"))
+	fmt.Println(c.Param("slug"))
+	id, ok := zeus.Groups[c.Param("slug")]
+	if !ok {
+		c.JSON(http.StatusNotAcceptable, "invalid group slug")
+		return
+	}
+
+	ics, err := client.GetICS(strconv.Itoa(id))
 	if err != nil {
 		zap.S().Error(err)
 		c.JSON(http.StatusNotAcceptable, err)
